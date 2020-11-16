fe4a8c39c7ba067d06fdfb9809082181a6d71675
Dylan Turner <dylantdmt@gmail.com>
2020-11-15 23:55:04 -0500

    Reimplemented basic parsers in haskell

--- src/BasicParser.hs
+++ src/BasicParser.hs
@@ -1,7 +1,38 @@
-module BasicParser(string) where
+module BasicParser(ident, float, integer, string) where
 
-import Debug.Trace(trace)
 import Parser
+
+-- <ident> ::= /[A-Za-z_][A-Za-z0-9_]+/
+ident :: Parser
+ident =
+    selectFrom  [ doParsers [ fstChrOpts, multiple latrChrOpts] Ident
+                , doParsers [ fstChrOpts ] Ident ]
+    where
+        fstChrOpts = selectFrom [ alpha, char '_' ]
+        latrChrOpts = selectFrom [ alpha, char '_', digit ]
+
+-- <float> ::= /-?((.[0-9]+)|([0-9]+.)|([0-9]+.[0-9]+))(e[+-]?[0-9]+)/
+float :: Parser
+float = 
+    scienceNum
+    where
+        numSteps = selectFrom
+            [ doParsers [ multiple digit, char '.', multiple digit ] Decimal
+            , doParsers [ multiple digit, char '.' ] Decimal 
+            , doParsers [ char '.', multiple digit ] Decimal ]
+        signedNum = selectFrom
+            [ doParsers [ selectFrom [ char '-', char '+' ], numSteps ] Decimal
+            , numSteps ]
+        scienceNum = selectFrom
+            [ doParsers [ signedNum, char 'e', integer ] Decimal
+            , signedNum ]
+        
+
+-- <int> ::= /-?[0-9]+/
+integer :: Parser
+integer = selectFrom
+    [ doParsers [ selectFrom [ char '-', char '+' ], multiple digit] IntegerType
+    , doParsers [ multiple digit ] IntegerType ]
 
 -- <string> ::= /'(\\.|[^\\'])*'/
 string :: Parser
